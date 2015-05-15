ec6a1f2498f787786120e1079e1332a58e2c186f
Gerardo Soares <gerardonsoares@gmail.com>
2015-05-15 00:06:40 -0300

    infra

--- fabfile/__init__.py
+++ fabfile/__init__.py
@@ -9,6 +9,6 @@ def go():
     env.site = 'facts.posdojo.us'
     env.hosts = [env.site]
     env.environment = 'production'
-    env.app_name = 'facts_as_a_service'
+    env.app_name = 'facts-as-a-service'
     env.RELEASES = Path('/opt').child(env.app_name, 'releases')
     env.GIT_URI = 'git@github.com:gerardon/facts-as-a-service.git'
